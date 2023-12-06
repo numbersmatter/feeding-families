3c9acd93b57f2aca1190b4825c5bc3efa69a76bd
MarkMutt <lleasy8@gmail.com>
2023-12-06 10:42:08 -0500

    login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,6 +3,7 @@ import { Link } from "@remix-run/react";
 import { UserAuthForm } from "~/components/auth/user-auth-form";
 import { buttonVariants } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
+import sideImage from "~/images/food-pantry.png"
 // import { Link, useActionData, useLoaderData, useSubmit } from "@remix-run/react";
 // import { useCallback, useState } from "react";
 // import { checkSessionCookie, signIn, signInWithToken } from "~/server/auth/auth.server";
@@ -58,8 +59,7 @@ export default function LoginPage() {
 
 
   return (
-
-    <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <Link
         to="/examples/authentication"
         className={cn(
@@ -70,7 +70,12 @@ export default function LoginPage() {
         Login
       </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src={sideImage}
+          alt=""
+        />
+        {/* <div className="absolute inset-0 bg-zinc-900" /> */}
         <div className="relative z-20 flex items-center text-lg font-medium">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -84,14 +89,12 @@ export default function LoginPage() {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          Acme Inc
+          Feeding Families
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than
-              ever before.&rdquo;
+              &ldquo;They were there for me and my children needed them.&rdquo;
             </p>
             <footer className="text-sm">Sofia Davis</footer>
           </blockquote>
@@ -128,7 +131,6 @@ export default function LoginPage() {
         </div>
       </div>
     </div>
-
   )
 
 }
